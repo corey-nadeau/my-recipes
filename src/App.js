73702c7ec1f3bb1fe388122a3f3a5acc6c26e217
corey.nadeau <djcjn16@hotmail.com>
2023-05-07 15:33:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const APP_ID = 'e6f9a24f';
+const APP_KEY = 'aac45bf4c79f6fca9f82f4efa1fdc876';
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState("");
+const [query, setQuery] = useState("smoked");
+useEffect(() => {
+	getRecipes();
+}, [query])
+const getRecipes = async () => {
+	const response = await fetch
+		(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+	const data = await response.json();
+	setRecipes(data.hits);
+	// console.log(data);
+
+};
+const updateSearch = e => {
+	setSearch(e.target.value);
+};
+const getSearch = e => {
+	e.preventDefault();
+	setQuery(search);
+	setSearch("");
+}
+
+return (
+  
+	<div className="App">
+    <div className='head'>
+      <h1>Corey's recipe search</h1>
+  </div>
+	<form className="search-form" onSubmit={getSearch} >
+		<input className="search-bar" type="text" value={search}
+			onChange={updateSearch} />
+		<button className="search-button" type="submit" >
+			Search
+		</button>
+	</form>
+
+	<div className="recipes">
+		{recipes.map(recipe => (
+		<Recipe
+			key={recipe.recipe.label}
+			title={recipe.recipe.label}
+			calories={recipe.recipe.calories}
+			image={recipe.recipe.image}
+			ingredients={recipe.recipe.ingredients}
+		/>
+
+		))}
+	</div>
+  <div className='search-form'>&copy; Corey Nadeau 2023 all rights reserved</div>
+
+	</div>
+);
 }
 
 export default App;
